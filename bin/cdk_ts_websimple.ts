93aa534dad17093f36493ae582eff2a951588937
flauts <hector.nieto@utec.edu.pe>
2024-09-12 10:32:48 -0500

    a

--- bin/cdk_ts_websimple.ts
+++ bin/cdk_ts_websimple.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { CdkTsWebsimpleStack } from '../lib/cdk_ts_websimple-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { CdkEc2Stack } from "../lib/cdk_ts_websimple-stack";
 
 const app = new cdk.App();
-new CdkTsWebsimpleStack(app, 'CdkTsWebsimpleStack', {
+new CdkEc2Stack(app, "CdkTsWebsimpleStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -15,7 +15,7 @@ new CdkTsWebsimpleStack(app, 'CdkTsWebsimpleStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: "172067734210", region: "us-east-1" },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- lib/cdk_ts_websimple-stack.ts
+++ lib/cdk_ts_websimple-stack.ts
@@ -1,16 +1,86 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {
+  Stack,
+  StackProps,
+  aws_ec2 as ec2,
+  aws_iam as iam,
+  CfnOutput,
+  DefaultStackSynthesizer,
+} from "aws-cdk-lib";
+import { Construct } from "constructs";
 
-export class CdkTsWebsimpleStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+export class CdkEc2Stack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    const synthesizer = new DefaultStackSynthesizer({
+      fileAssetsBucketName: "cf-templates-iw9mos24h2jo-us-east-1",
+      bucketPrefix: "",
+      cloudFormationExecutionRole: "arn:aws:iam::172067734210:role/LabRole",
+      deployRoleArn: "arn:aws:iam::172067734210:role/LabRole",
+      fileAssetPublishingRoleArn: "arn:aws:iam::172067734210:role/LabRole",
+      deployRoleExternalId: "arn:aws:iam::172067734210:role/LabRole",
+      imageAssetPublishingRoleArn: "arn:aws:iam::172067734210:role/LabRole",
+    });
 
-    // The code that defines your stack goes here
+    super(scope, id, { ...props, synthesizer });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkTsWebsimpleQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const vpc = ec2.Vpc.fromLookup(this, "vpc", {
+      vpcId: "vpc-00efc54137e6a9ef2",
+    });
+
+    const secGroup = ec2.SecurityGroup.fromSecurityGroupId(
+      this,
+      "launch-wizard-1",
+      "sg-0c77723568ab75889"
+    );
+
+    const keyPair = ec2.KeyPair.fromKeyPairName(
+      this,
+      "ExistingKeyPair",
+      "vockey"
+    );
+
+    const labRole = iam.Role.fromRoleArn(
+      this,
+      "LabRole",
+      "arn:aws:iam::172067734210:role/LabRole"
+    );
+
+    const userData = ec2.UserData.forLinux();
+    userData.addCommands(
+      "git clone https://github.com/flauts/webplantilla.git /var/www/html/webplantilla",
+      "git clone https://github.com/flauts/websimple.git /var/www/html/websimple"
+    );
+
+    const instance = new ec2.Instance(this, "mv-cdk", {
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.MICRO
+      ),
+      machineImage: ec2.MachineImage.genericLinux({
+        "us-east-1": "ami-0aa28dab1f2852040",
+      }),
+      vpc,
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+      securityGroup: secGroup,
+      keyPair,
+      userData,
+      role: labRole,
+    });
+
+    new CfnOutput(this, "InstanceIdOutput", {
+      value: instance.instanceId,
+      exportName: "InstanceId",
+    });
+    new CfnOutput(this, "InstancePublicIPOutput", {
+      value: instance.instancePublicIp || "No public IP assigned",
+      exportName: "InstancePublicIP",
+    });
+    new CfnOutput(this, "WebsimpleURLOutput", {
+      value: `http://${instance.instancePublicIp}/websimple`,
+      exportName: "WebsimpleURL",
+    });
+    new CfnOutput(this, "WebPlantillaURLOutput", {
+      value: `http://${instance.instancePublicIp}/webplantilla`,
+      exportName: "WebplantillaURL",
+    });
   }
 }
